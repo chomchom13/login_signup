ed7283ffce20751b36a02a8ae5c020d292d7aab7
chomchom13 <chawhannaman5@gmail.com>
2024-02-03 17:59:48 +0530

    added delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -12,6 +12,9 @@ import {
   updateUserStart,
   updateUserFailure,
   updateUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
 } from "../redux/user/userSlice";
 
 export default function Profile() {
@@ -81,6 +84,23 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/server/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json()
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -132,16 +152,23 @@ export default function Profile() {
           id="password"
           onChange={handleChange}
         />
-        <button disabled={loading} className="bg-grey-slate-3 text-white-1 rounded-lg p-3 uppercase hover:backdrop-opacity-95 disabled:backdrop-opacity-80">
-          {loading? "Loading...": "Update"}
+        <button
+          disabled={loading}
+          className="bg-grey-slate-3 text-white-1 rounded-lg p-3 uppercase hover:backdrop-opacity-95 disabled:backdrop-opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-1 cursor-pointer">Delete Account</span>
+        <span onClick={handleDeleteUser} className="text-red-1 cursor-pointer">
+          Delete Account
+        </span>
         <span className="text-red-1 cursor-pointer">Sign Out</span>
       </div>
-      <p className="mt-5 text-red-1">{error? error : ''}</p>
-      <p className="mt-5 text-green-2">{updateSuccess? 'User is Updated Successfully!' : ''}</p>
+      <p className="mt-5 text-red-1">{error ? error : ""}</p>
+      <p className="mt-5 text-green-2">
+        {updateSuccess ? "User is Updated Successfully!" : ""}
+      </p>
     </div>
   );
 }
